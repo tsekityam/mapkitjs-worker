88c6c3587ab785d8c83c041f25d86de24fa04ef0
Tse Kit Yam <me@kytse.com>
2021-01-22 00:04:50 +0800

    Enable github actions

--- src/jwt.js
+++ src/jwt.js
@@ -1,20 +1,16 @@
 var jwt = require("jsonwebtoken");
-var url = require("url");
 
 async function handleRequest(request) {
-  var payload = {
-    origin: url.parse(request.url).hostname
-  };
+  var payload = { origin: new URL(request.url).origin };
 
   var token = jwt.sign(payload, MAPKIT_PRIVATE_KEY, {
     algorithm: "ES256",
-    expiresIn: "60m",
+    expiresIn: "1h",
     issuer: TEAM_ID,
     keyid: KEY_ID
   });
 
   const res = new Response(token);
-  res.headers.set("access-control-allow-origin", "same-origin");
   return res;
 }
 
